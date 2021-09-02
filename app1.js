d0029ba5c791bf99f6874c306461e9aa0ee076de
Christopher Harden <Harden.Christopher00@gmail.com>
2021-09-02 14:38:55 -0500

    had to add some fun to the page

---
+++ app1.js
@@ -0,0 +1,10 @@
+console.log('hello American People')
+
+let favColor = prompt('Whats your favorite Color?: ');
+
+if(favColor == 'red white blue'){
+    document.write('Welcome Brother ' );
+} else {
+    document.body.style.background=favColor
+}
+
---
+++ app2.js
@@ -0,0 +1,7 @@
+let loki = confirm("Is this page getting you down? If so click 'OK' to be taken away from this page. Click 'Cancel' to stay and continue reading.")
+
+if(loki) {
+window.location.href="https://youtu.be/dQw4w9WgXcQ"
+} else {
+alert("Good, people are dying be an adult")
+}
