dd9601a99a63af9772ca889fd067502bf931b39e
Christopher Harden <Harden.Christopher00@gmail.com>
2021-09-03 00:05:58 -0500

    needed to add last part of functions for lab assignment

--- app1.js
+++ app1.js
@@ -1,9 +1,35 @@
-console.log('hello American People')
+console.log("hello American People");
 
-let favColor = prompt('Whats your favorite Color?: ');
+let favColor = prompt("Whats your favorite Color?: ");
 
-if(favColor == 'red white blue'){
-    document.write('Welcome Brother ' );
+if (favColor == "red white blue") {
+  document.write("Welcome Brother ");
 } else {
-    document.body.style.background=favColor
+  document.body.style.background = favColor;
+}
+
+for (let i = 1; i <= numberOfAttempts; i++) {
+  userAnswer = prompt("Guess a number between 1 and 100");
+
+  while (userAnswer < 1 || userAnswer > 100) {
+    userAnswer = prompt("Guess a number between 1 and 100");
+  }
+  if (userAnswer == correctAnswer) {
+    alert(
+      "You got it right. You have " + (numberOfAttempts - i) + " attempts left"
+    );
+    break;
+  } else if (userAnswer < correctAnswer) {
+    alert(
+      "Your answer is to low.  Try Again! You have " +
+        (numberOfAttempts - i) +
+        " attempts left"
+    );
+  } else if (userAnswer > correctAnswer) {
+    alert(
+      "Your answer is to high.  Try Again! You have " +
+        (numberOfAttempts - i) +
+        " attempts left"
+    );
+  }
 }
--- app3.js
+++ app3.js
@@ -1,35 +1,60 @@
-console.log('hello American People')
+console.log("hello American People");
 
-let favColor = prompt('Whats your favorite Color?: ');
+let favColor = prompt("Whats your favorite Color?: ");
 
-if(favColor == 'red white blue'){
-    document.write('Welcome Brother ' );
+if (favColor == "red white blue") {
+  document.write("Welcome Brother ");
 } else {
-    document.body.style.background=favColor
+  document.body.style.background = favColor;
 }
 
-let loki = confirm("Is this page getting you down? If so click 'OK' to be taken away from this page. Click 'Cancel' to stay and continue reading.")
+let loki = confirm(
+  "Is this page getting you down? If so click 'OK' to be taken away from this page. Click 'Cancel' to stay and continue reading."
+);
 
-if(loki) {
-window.location.href="https://youtu.be/dQw4w9WgXcQ"
+if (loki) {
+  window.location.href = "https://youtu.be/dQw4w9WgXcQ";
 } else {
-alert("Good, people are dying be an adult")
+  alert("Good, people are dying be an adult");
 }
 
-function changeToRed(){
-    document.body.style.background="red"
+function changeToRed() {
+  document.body.style.background = "red";
 }
 
-function changeToBlue(){
-    document.body.style.background="blue"
+function changeToBlue() {
+  document.body.style.background = "blue";
 }
 
-function changeToWhite(){
-    document.body.style.background="white"
+function changeToWhite() {
+  document.body.style.background = "white";
 }
 
-function snowFlakeTest (){
-    window.setInterval(changeToRed, 1000)
-    window.setInterval(changeToWhite, 1500)
-    window.setInterval(changeToBlue, 2000)
+function snowFlakeTest() {
+  window.setInterval(changeToRed, 1000);
+  window.setInterval(changeToWhite, 1500);
+  window.setInterval(changeToBlue, 2000);
 }
+
+function userPrompt() {
+  return prompt("How dangerous is Afghanistan today? Rate it in 1- 5 aks.");
+}
+function isNumberValid(value) {
+  return Number.isInteger(Number(value)) && value >= 1 && value <= 5;
+}
+
+function akCounter() {
+  let aks = userPrompt();
+  console.log(aks);
+  console.log(isNumberValid(aks));
+  while (!isNumberValid(aks)) {
+    aks = userPrompt();
+  }
+  for (let i = 0; i < aks; i++) {
+    document.write(
+      '<img src="https://media.istockphoto.com/photos/ak-47-picture-id177391994?s=612x612">'
+    );
+  }
+}
+
+akCounter();
